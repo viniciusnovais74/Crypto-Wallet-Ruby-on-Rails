73071bcb1297fbd350d80d0e0459e6635091f00f
Vinicius Novais <viniciusnovais74@gmail.com>
2022-03-01 18:48:18 -0300

    Finalizando Modulo 3 do curso de Ruby e preparando para o Heroku

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:curso] ='Curso de Ruby on Rails'
+    session[:user_name] = 'MarkDown'
     @nome = params[:nome]
   end
 end
